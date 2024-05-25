1a93d8d4ed2a2c2844bb890687d6408786c257c2
Kiri Choi <kirichoi@uw.edu>
2024-05-25 17:26:52 -0400

    Remove unused model

--- Figures/Fig6-MI_timing_ORN/gen_lag.py
+++ Figures/Fig6-MI_timing_ORN/gen_lag.py
@@ -20,57 +20,6 @@ from models import *
 
 SAVE = False
 
-#%% ORN model
-
-N = 10
-
-dt1 = 0.1
-odor_tau = 500
-rate_sigma = 0
-smooth_T = 20
-num_ticks = 1_000_000
-num_rate_bins = 100
-num_stim_bins = 100
-tau_eps = 300
-tau_rate = 50
-cutout = 20000
-
-mus = np.logspace(0.65, 1.2, 10)
-sigma = 0.4
-lags_ORN = []
-acc_ORN = []
-
-seed = np.random.randint(1000, size=N)
-
-for im, mu in enumerate(mus):
-    print(mu, sigma)
-    lags_ORN_t = []
-    acc_ORN_t = []
-    for i in range(N):
-        a = ORN_model(dt=dt1, N=num_ticks, odor_tau=odor_tau, odor_mu=mu, 
-                        odor_sigma=sigma, rate_sigma=rate_sigma,
-                        k_rate=1./tau_rate)
-        a.tau_eps = tau_eps
-        a.gen_sig_trace_OU(seed=seed[i], smooth_T=smooth_T)
-        a.integrate()
-        a.calc_rate()
-        
-        stims = a.stim[int(cutout/dt1):]
-        rates = a.rate[int(cutout/dt1):]
-        
-        acc = scipy.signal.correlate(stims-np.mean(stims), rates-np.mean(rates))
-        acc_norm = acc/(np.std(stims)*np.std(rates)*len(stims))
-        acc_ORN_t.append(acc_norm[int(cutout/dt1):])
-        acc_lag = scipy.signal.correlation_lags(len(stims), len(rates))
-        lags_ORN_t.append(acc_lag[np.argmax(acc)])
-    
-    acc_ORN.append(acc_ORN_t)
-    lags_ORN.append(lags_ORN_t)
-    
-if SAVE:
-    file = './data/ORN_lag_3.npy'
-    np.save(file, lags_ORN)
-
 #%% Na+K+Ca model
 
 N = 10
